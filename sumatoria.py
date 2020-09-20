18dd17345187e7ce5ac80c8bf99847c010d86b78
Andres <andres.65alejandro@gmail.com>
2020-09-20 16:33:33 -0600

    Tarea 3 Sumatoria

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,17 @@
 def sumatoria_cubica(n):
-    raise NotImplementedError()
+    suma =0
+    for i in range (1,n+1):
+        for j in range(1,i+1):
+            for k in range(j,i+j+1):
+                suma+=1
+    return int(suma)
+    raise  NotImplementedError()
+
 
 def sumatoria_constante(n):
-    raise NotImplementedError()
+    k = n*(n+1)
+    suma =((n+2)/3) *k
+    return int(suma)
+    raise  NotImplementedError()
+
 
